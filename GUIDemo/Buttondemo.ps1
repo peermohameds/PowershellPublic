51232d0b94452cea94ca5f1e8da837e2ae3ee5ed
Peermohamdes <peermohameds@gmail.com>
2019-08-15 11:30:15 +0530

    saved with bootstrap design

--- GUIDemo/Buttondemo.ps1
+++ GUIDemo/Buttondemo.ps1
@@ -90,160 +90,100 @@ $htmllayout=@"
 `$(document).ready(function(){
       var numOfVisibleRows = `$("#mytable tr:visible").length;
       `$("#resbox").val(numOfVisibleRows);
-      
-  `$("#myInput").on("keyup", function() {
-     var value = `$(this).val().toLowerCase();
-		`$("#mytable tr").filter(function() {
-			`$(this).toggle(`$(this).text().toLowerCase().indexOf(value) > -1);
-			var numOfVisibleRows = `$("#mytable tr:visible").length;
-			`$("#resbox").val(numOfVisibleRows);
-		});
-	});
+      `$("#searchbtn").click(function(){
+          var value = `$("#myInput").val().toLowerCase();
+          alert(value)
+          `$("#mytable tr").filter(function() {
+              `$(this).toggle(`$(this).text().toLowerCase().indexOf(value) > -1)
+            });
+        var numOfVisibleRows = `$("#mytable tr:visible").length;
+        `$("#resbox").val(numOfVisibleRows);
+      });
 });
 </script>
-<script>
-`$(document).ready(function() {
 
-  function exportTableToCSV(`$table, filename) {
-
-    var `$rows = `$table.find('tr:has(td),tr:has(th)'),
-
-        // Temporary delimiter characters unlikely to be typed by keyboard
-        // This is to avoid accidentally splitting the actual contents
-        tmpColDelim = String.fromCharCode(11), // vertical tab character
-        tmpRowDelim = String.fromCharCode(0), // null character
-
-        // actual delimiter characters for CSV format
-        colDelim = '","',
-        rowDelim = '"\r\n"',
-
-        // Grab text from table into CSV formatted string
-        csv = '"' + `$rows.map(function (i, row) {
-            var `$row = `$(row), `$cols = `$row.find('td,th');
-
-            return `$cols.map(function (j, col) {
-                var `$col = `$(col), text = `$col.text();
-
-                return text.replace(/"/g, '""'); // escape double quotes
-
-            }).get().join(tmpColDelim);
-
-        }).get().join(tmpRowDelim)
-            .split(tmpRowDelim).join(rowDelim)
-            .split(tmpColDelim).join(colDelim) + '"',
-
-
-
-        // Data URI
-        csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
-
-        console.log(csv);
-
-        if (window.navigator.msSaveBlob) { // IE 10+
-            alert('IE' + csv);
-            window.navigator.msSaveOrOpenBlob(new Blob([csv], {type: "text/plain;charset=utf-8;"}), "csvname.csv")
-        } 
-        else {
-            `$(this).attr({ 'download': filename, 'href': csvData, 'target': '_blank' }); 
-        }
-}
-  // This must be a hyperlink
-  `$("#exportcsv").on('click', function(event) {
-    // CSV
-    var args = [`$("#dtBasicExample"), 'export.csv'];
-    
-    exportTableToCSV.apply(this, args);
-
-    // If CSV, don't do event.preventDefault() or return false
-    // We actually need this to be a typical hyperlink
-  });
-});
-</script>
 <script>
 `$(document).ready(function(){
-  `$("exportxls").click(function(){
-    alert ("export xls clicked"); 
-  });
-
+  `$("#exportcsv").click(function(){
+    var data = `$("#dtBasicExample").first(); //Only one table
+    var csvData = [];
+    var tmpArr = [];
+    var tmpStr = '';
+    data.find("tr:visible").each(function() {
+        if(`$(this).find("th").length) {
+            `$(this).find("th").each(function() {
+              tmpStr = `$(this).text().replace(/"/g, '""');
+              tmpArr.push('"' + tmpStr + '"');
+            });
+            csvData.push(tmpArr);
+        } else {
+            tmpArr = [];
+               `$(this).find("td").each(function() {
+                  if(`$(this).text().match(/^-{0,1}\d*\.{0,1}\d+`$/)) {
+                      tmpArr.push(parseFloat(`$(this).text()));
+                  } else {
+                      tmpStr = `$(this).text().replace(/"/g, '""');
+                      tmpArr.push('"' + tmpStr + '"');
+                  }
+              });
+            csvData.push(tmpArr.join(','));
+        }
+    });
+    var output = csvData.join('\n');
+    
+    //alert(output);
+    var uri = 'data:application/csv;charset=UTF-8,' + encodeURIComponent(output);
+    if (window.navigator.msSaveBlob) { // IE 10+
+      //alert('IE' + output);
+      window.navigator.msSaveOrOpenBlob(new Blob([output], {type: "text/plain;charset=utf-8;"}), "csvname.csv")
+      } 
+    else {
+      `$(this).attr({ 'download': 'export.csv', 'href': uri, 'target': '_blank' }); 
+      }
+  })
+  
 });
 </script>
 <style>
-* {
-  box-sizing: border-box;
-}
-
 body {
   font-family: Arial, Helvetica, sans-serif;
 }
-
-/* Style the header */
-header {
-  background-color: #666;
-  padding: 30px;
-  text-align: center;
-  font-size: 35px;
-  color: white;
-}
-
-/* Create two columns/boxes that floats next to each other */
-nav {
-  float: left;
-  width: 30%;
-  height: 300px; /* only for demonstration, should be removed */
-  background: #ccc;
-  padding: 20px;
-}
-
-/* Style the list inside the menu */
-nav ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-article {
-  float: left;
-  padding: 20px;
-  width: 70%;
-  background-color: #f1f1f1;
-  height: 300px; /* only for demonstration, should be removed */
-}
-
-/* Clear floats after the columns */
-section:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-/* Style the footer */
-footer {
-  background-color: #777;
-  padding: 10px;
-  text-align: center;
-  color: white;
-}
-
-/* Responsive layout - makes the two columns/boxes stack on top of each other instead of next to each other, on small screens */
-@media (max-width: 600px) {
-  nav, article {
-    width: 100%;
-    height: auto;
-  }
-}
+.table{  font-size: 12px;}
+.table:hovered { background-color: "red"; color: "white"; }
 </style>
-
 </head>
 <body>
 <h2>Filterable Table</h2>
 <p>Type something in the input field to search the table.</p>  
-<input id="myInput" type="text" placeholder="Search..">
-No of records : <input id="resbox" type="text" placeholder="# records">
-<br><br>
-Export Table as :
-<Button id="exportcsv" class="btn btn-primary btn-sm">CSV</button>
-<Button id="exportxls" class="btn btn-success btn-sm">XLS</button>
-<Button id="exportpdf" class="btn btn-info btn-sm">PDF</button>
-<br>
+<div class="row">
+      <div class="col">
+            <div class="input-group input-group-sm mb-3">
+                  <input type="text" class="form-control" id="myInput" placeholder="Search.No wildcards">
+                  <div class="input-group-append">
+                    <button class="btn btn-success btn-sm" id="searchbtn" type="submit">Search</button> 
+                  </div>
+            </div>
+      </div>
+      <div class="col">
+            <div class="input-group input-group-sm mb-3">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"># of Rows</span>
+                  </div>
+                  <input type="text" class="form-control" id="resbox" placeholder="#" > 
+            </div>
+      </div>
+      
+      <div class="col"></div>
+      <div class="col">
+            <div class="btn-group btn-group-sm">
+                  <Button id="exportcsv" class="btn btn-primary">CSV</button>
+                  <Button id="exportxls" class="btn btn-success">XLS</button>
+                  <Button id="exportpdf" class="btn btn-info">PDF</button>
+            </div>
+      </div>
+      <div class="col"></div>
+</div>
+
 </body>
 </html>
 "@
@@ -256,15 +196,17 @@ $WebForm = [Windows.MarkUp.XamlReader]::Load($FormReader)
 $Xaml.SelectNodes("//*[@Name]") | %{Set-Variable -Name $_.Name -Value $WebForm.FindName($_.Name)}
 
 $Btn.Add_Click({
-       
-  [xml]$Services = Get-Service | Select-Object Name,DisplayName,Status,StartType | ConvertTo-Html -As Table -Fragment
+  $result = Get-Service | Select-Object Name,DisplayName,Status,StartType      
+  #$result = Get-Process | Select -First 20      
+ 
+  [xml]$Services = $result  | ConvertTo-Html -As Table -Fragment
   $col=for($i=2; $i -le $Services.table.ChildNodes.Count-1;$i++){ "<tr>"+$Services.table.ChildNodes[$i].InnerXml+"</tr>" }
   $thead="<thead><tr>"+$Services.table.ChildNodes[1].InnerXml+"</tr></thead>"
   $col ="<tbody id=`"mytable`">"+$col+"</tbody>"
   $temp =$thead+$col
   $Services.table.InnerXml=$temp
   $class = $Services.CreateAttribute("class")
-  $class.value = 'table'
+  $class.value = 'table table-sm'
   $ID = $Services.CreateAttribute("id")
   $ID.value = 'dtBasicExample'
   $Services.table.Attributes.Append($class)
