98fd66f066e9435bb8476a3c7b9369481c83500d
Peermohamdes <peermohameds@gmail.com>
2019-08-15 06:59:31 +0530

    added export csv and bootstrap designs

--- GUIDemo/Buttondemo.ps1
+++ GUIDemo/Buttondemo.ps1
@@ -27,7 +27,7 @@
        
         <StackPanel>
         <Button Name="Btn" Width ="80" Height="30">Click Me</Button>
-        <WebBrowser Name="mybrowser" Margin="5 5 5 5"/> 
+        <WebBrowser Name="mybrowser" Margin="5 5 5 5" MinWidth="300" MinHeight="500"/> 
         </StackPanel>
 </Window>
 "@
@@ -72,15 +72,101 @@ div {
 </body>
 </html>
 "@
+$jspath='D:\jquery\jquery-3.4.1.min.js'
 
 $htmllayout=@"
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge" /> 
-<title>CSS Template</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script>
+`$(document).ready(function(){
+      var numOfVisibleRows = `$("#mytable tr:visible").length;
+      `$("#resbox").val(numOfVisibleRows);
+      
+  `$("#myInput").on("keyup", function() {
+     var value = `$(this).val().toLowerCase();
+		`$("#mytable tr").filter(function() {
+			`$(this).toggle(`$(this).text().toLowerCase().indexOf(value) > -1);
+			var numOfVisibleRows = `$("#mytable tr:visible").length;
+			`$("#resbox").val(numOfVisibleRows);
+		});
+	});
+});
+</script>
+<script>
+`$(document).ready(function() {
+
+  function exportTableToCSV(`$table, filename) {
+
+    var `$rows = `$table.find('tr:has(td),tr:has(th)'),
+
+        // Temporary delimiter characters unlikely to be typed by keyboard
+        // This is to avoid accidentally splitting the actual contents
+        tmpColDelim = String.fromCharCode(11), // vertical tab character
+        tmpRowDelim = String.fromCharCode(0), // null character
+
+        // actual delimiter characters for CSV format
+        colDelim = '","',
+        rowDelim = '"\r\n"',
+
+        // Grab text from table into CSV formatted string
+        csv = '"' + `$rows.map(function (i, row) {
+            var `$row = `$(row), `$cols = `$row.find('td,th');
+
+            return `$cols.map(function (j, col) {
+                var `$col = `$(col), text = `$col.text();
+
+                return text.replace(/"/g, '""'); // escape double quotes
+
+            }).get().join(tmpColDelim);
+
+        }).get().join(tmpRowDelim)
+            .split(tmpRowDelim).join(rowDelim)
+            .split(tmpColDelim).join(colDelim) + '"',
+
+
+
+        // Data URI
+        csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+
+        console.log(csv);
+
+        if (window.navigator.msSaveBlob) { // IE 10+
+            alert('IE' + csv);
+            window.navigator.msSaveOrOpenBlob(new Blob([csv], {type: "text/plain;charset=utf-8;"}), "csvname.csv")
+        } 
+        else {
+            `$(this).attr({ 'download': filename, 'href': csvData, 'target': '_blank' }); 
+        }
+}
+  // This must be a hyperlink
+  `$("#exportcsv").on('click', function(event) {
+    // CSV
+    var args = [`$("#dtBasicExample"), 'export.csv'];
+    
+    exportTableToCSV.apply(this, args);
+
+    // If CSV, don't do event.preventDefault() or return false
+    // We actually need this to be a typical hyperlink
+  });
+});
+</script>
+<script>
+`$(document).ready(function(){
+  `$("exportxls").click(function(){
+    alert ("export xls clicked"); 
+  });
+
+});
+</script>
 <style>
 * {
   box-sizing: border-box;
@@ -145,37 +231,19 @@ footer {
   }
 }
 </style>
+
 </head>
 <body>
-
-<h2>CSS Layout Float</h2>
-<p>In this example, we have created a header, two columns/boxes and a footer. On smaller screens, the columns will stack on top of each other.</p>
-<p>Resize the browser window to see the responsive effect (you will learn more about this in our next chapter - HTML Responsive.)</p>
-
-<header>
-  <h2>Cities</h2>
-</header>
-
-<section>
-  <nav>
-    <ul>
-      <li><a href="#">London</a></li>
-      <li><a href="#">Paris</a></li>
-      <li><a href="#">Tokyo</a></li>
-    </ul>
-  </nav>
-  
-  <article>
-    <h1>London</h1>
-    <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
-    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
-  </article>
-</section>
-
-<footer>
-  <p>Footer</p>
-</footer>
-
+<h2>Filterable Table</h2>
+<p>Type something in the input field to search the table.</p>  
+<input id="myInput" type="text" placeholder="Search..">
+No of records : <input id="resbox" type="text" placeholder="# records">
+<br><br>
+Export Table as :
+<Button id="exportcsv" class="btn btn-primary btn-sm">CSV</button>
+<Button id="exportxls" class="btn btn-success btn-sm">XLS</button>
+<Button id="exportpdf" class="btn btn-info btn-sm">PDF</button>
+<br>
 </body>
 </html>
 "@
@@ -188,12 +256,25 @@ $WebForm = [Windows.MarkUp.XamlReader]::Load($FormReader)
 $Xaml.SelectNodes("//*[@Name]") | %{Set-Variable -Name $_.Name -Value $WebForm.FindName($_.Name)}
 
 $Btn.Add_Click({
-        #$uri = [uri]::new("https://www.google.com")
+       
+  [xml]$Services = Get-Service | Select-Object Name,DisplayName,Status,StartType | ConvertTo-Html -As Table -Fragment
+  $col=for($i=2; $i -le $Services.table.ChildNodes.Count-1;$i++){ "<tr>"+$Services.table.ChildNodes[$i].InnerXml+"</tr>" }
+  $thead="<thead><tr>"+$Services.table.ChildNodes[1].InnerXml+"</tr></thead>"
+  $col ="<tbody id=`"mytable`">"+$col+"</tbody>"
+  $temp =$thead+$col
+  $Services.table.InnerXml=$temp
+  $class = $Services.CreateAttribute("class")
+  $class.value = 'table'
+  $ID = $Services.CreateAttribute("id")
+  $ID.value = 'dtBasicExample'
+  $Services.table.Attributes.Append($class)
+  $Services.table.Attributes.Append($ID)
+  ConvertTo-Html -Body $Services.InnerXml -Head $htmllayout | Out-File D:\sample.html -Force 
+        $uri = [uri]::new("D:\sample.html")
         #$mybrowser.Navigate($uri)
         #$uri
-        #$mybrowser.NavigateToString($htmllayout)
-        $mybrowser.Document.getElementsByTagName("text").value="Sample"
-        
+        $mybrowser.Navigate($uri)
+        #$mybrowser.NavigateToString($uri)
 })
 #$mybrowser.Navigate("www.google.com")
 
--- HTML Output/HTMLtablewithJqueryFilter.ps1
+++ HTML Output/HTMLtablewithJqueryFilter.ps1
@@ -6,13 +6,25 @@ $Head =@"
 <script>
 `$(document).ready(function(){
   `$("#myInput").on("keyup", function() {
-    var value = `$(this).val().toLowerCase();
-    `$("#mytable tr").filter(function() {
-      `$(this).toggle(`$(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
+		var value = `$(this).val().toLowerCase();
+		`$("#mytable tr").filter(function() {
+			`$(this).toggle(`$(this).text().toLowerCase().indexOf(value) > -1);
+			var numOfVisibleRows = `$("#mytable tr:visible").length;
+			`$("#resbox").val(numOfVisibleRows);
+		});
+  
+	});
+	`$("#mybtn").click(function(){
+		alert ("Hello");	
+	});
 });
 </script>
+<script>
+`$(document).ready(function(){
+	
+});
+</script>
+
 <script src="sortable.js"></script>
 <script>
 `$(document).ready(function () {
@@ -63,153 +75,21 @@ th {
     color: green;
     font-weight: bold;
 }
-.sortable {
-	margin: 0;
-	padding: 0;
-	border-collapse: collapse;
-	border-style: none;
-	border-width: 0;
-	border-spacing: 0;
-}
-.sortable thead,
-.sortableth2 {
-	height: 40px;
-	line-height: 40px;
-	background: #effbf8;
-	white-space: nowrap;
-	overflow: hidden;
-}
-.sortableth2 {
-	box-shadow: 0px 5px 5px rgba( 0,0,0,0.1 ) inset, 
-		0px 5px 5px rgba( 0,0,0,0.5 );
-	opacity: 0.95;
-}
-.sortable tr {
-	height: 30px;
-	cursor: default;
-}
-.sortableth2 a,
-.sortable td {
-	padding: 0 3px;
-}
-
-.sortable thead td { /* ios safari 'thead', 'tbody' - not support 'box-shadow' */
-	box-shadow: 0px 9px 5px -6px rgba( 0,0,0,0.1 ) inset;
-}
-.sortable thead td {
-/*	font-weight: bold;
-	text-align: center;*/ /* default */
-}
-.sortable tbody {
-/*	background: #fff;*/
-}
-
-/* reponsive ------------------------------------------------------------------------------------------------------------------------------------- */
-/*
-	min-width: 0 !important - required for column with 'max-width'
-	:nth-child( n ) - column# start with left 'tdpad', n=1, table 1st column, n=2
-*/
-
-@media( max-width: 569px ) {
-/*	.sortable td:nth-child( 4 ) { min-width: 0 !important; display: none !important; }
-	.sortable td:nth-child( 5 ) { min-width: 0 !important; max-width: 300px; }*/
-}
-@media( max-width: 414px ) {
-/*	.sortable td:nth-child( 3 ) { min-width: 0 !important; display: none !important; }
-	.sortable td:nth-child( 5 ) { min-width: 0 !important; max-width: 220px; }*/
-}
-@media( max-width: 320px ) {
-/*	.sortable td:nth-child( 6 ) { display: none !important; }
-	.sortable td:nth-child( 5 ) { min-width: 0 !important; max-width: 200px; }*/
-}
-@media( max-height: 414px ) {
-	.sortable thead td {
-		border-bottom: 1px solid rgba( 0,0,0,0.05 );
-	}
-}
-
-
-/* SHOULD NOT change below this line ------------------------------------------------------------------------------------------- */
-.sortableth2 a,
-.sortable td { /* 3 lines for ellipsis: 'td max-width' must be set */
-	white-space: nowrap;
-	overflow: hidden;
-	text-overflow: ellipsis;
-}
-.sortable tbody tr:nth-child( even ) { /* zebra stripe */
-	background: rgba( 0,0,0,0.05 );
-}
-.sortable tbody tr:hover {
-	background: rgba( 0,0,0,0.15 );
-}
-.sorted { /* sorted column */
-	background: rgba( 0,0,0,0.05 );
-}
-.asc:before, .asctmp:before {
-	content: '▲';
-	color: green;
-}
-.desc:before {
-	content: '▼';
-	color: crimson;
-}
-.asctmp:before {
-	opacity: 0;
-}
-/* custom <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
-
-/* DO NOT change below this line ---------------------------------------------------------------------------------------------------- */
-body {
-	width: 100% !important;
-}
-.divbefore,
-.divafter {
-	position: fixed;
-	z-index: 10;
-}
-.divafter {
-	bottom: 0;
-}
-.sortable thead,
-.sortableth2 {
-	cursor: pointer;
-	user-select: none;
-}
-.sortableth2 {
-	position: fixed;
-	width: 100%;
-	z-index: 1;
-}
-.sortableth2 a {
-	display: inline-block;
-}
-.sortable thead {
-	visibility: hidden;
-}
-.sortable thead td:empty {
-	pointer-events: none;
-}
-.sortable tbody {
-	overflow: auto;
-	-webkit-overflow-scrolling: touch; /* ios momentum scroll */
-}
-.sortable .tdpad {
-	width: 50%; /* oversize for centering table */
-	padding: 0;
-}
 </Style>
 </head>
 <body>
 <h2>Filterable Table</h2>
-<p>Type something in the input field to search the table for first names, last names or emails:</p>  
+<p>Type something in the input field to search the table.</p>  
 <input id="myInput" type="text" placeholder="Search..">
+No of records : <input id="resbox" type="text" placeholder="# records">
 <br><br>
+<Button id="mybtn">Copy</button>
 </body>
 </html>
 "@
 
 #[xml]$Services = Get-Service | Select-Object Name,DisplayName,Status | ConvertTo-Html -As Table -Fragment
-[xml]$Services = Get-ChildItem C:\Windows | Select-Object -First 20 | ConvertTo-Html -As Table -Fragment
+[xml]$Services = Get-Service | Select-Object Name,DisplayName,Status,StartType -First 20 | ConvertTo-Html -As Table -Fragment
 $col=for($i=2; $i -le $Services.table.ChildNodes.Count-1;$i++){ "<tr>"+$Services.table.ChildNodes[$i].InnerXml+"</tr>" }
 $thead="<thead><tr>"+$Services.table.ChildNodes[1].InnerXml+"</tr></thead>"
 $col ="<tbody id=`"mytable`">"+$col+"</tbody>"
