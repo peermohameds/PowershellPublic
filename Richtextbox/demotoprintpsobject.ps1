bd6babce702b5ca6da90a7568e60ac9aedd56ead
Peermohamdes <peermohameds@gmail.com>
2019-09-19 10:50:16 +0530

    commit

--- Richtextbox/demotoprintpsobject.ps1
+++ Richtextbox/demotoprintpsobject.ps1
@@ -7,7 +7,7 @@
         <Button Name="Btn" Width ="80" Height="30" Margin="10 0">Load Text</Button>
         <Button Name="NxtBtn" Width ="80" Height="30" Margin="10 0">Next</Button>
         </WrapPanel>
-        <RichTextBox Name='Rtxt' FontFamily ="Lucida Console" HorizontalScrollBarVisibility = "Auto" 
+        <RichTextBox Name='Rtxt' FontFamily ="Consolas" HorizontalScrollBarVisibility = "Auto" 
             VerticalScrollBarVisibility ="Auto" HorizontalAlignment="Left" Height="330" 
                 VerticalAlignment="Top" Width="600" Margin ="10"/>  
         
@@ -16,6 +16,150 @@
 </Window>
 "@
 
+<# [XML]$doc=@"
+<FlowDocument xml:space="preserve" xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation">
+
+    <Table CellSpacing="5">
+
+      <Table.Columns>
+        <TableColumn/>
+        <TableColumn/>
+        <TableColumn/>
+        <TableColumn/>
+      </Table.Columns>
+
+      <TableRowGroup>
+
+        <!-- Title row for the table. -->
+        <TableRow Background="SkyBlue" Foreground="White">
+          <TableCell ColumnSpan="4" TextAlignment="Center">
+            <Paragraph FontSize="24pt" FontWeight="Bold" ToolTip="New Item Peer">Planetary Information</Paragraph>
+          </TableCell>
+        </TableRow>
+
+        <!-- Header row for the table. -->
+        <TableRow Background="LightGoldenrodYellow">
+          <TableCell BorderThickness="1" BorderBrush="Red" TextAlignment="Center"><Paragraph FontSize="14pt" FontWeight="Bold">Planet</Paragraph></TableCell>
+          <TableCell><Paragraph FontSize="14pt" FontWeight="Bold">Mean Distance from Sun</Paragraph></TableCell>
+          <TableCell><Paragraph FontSize="14pt" FontWeight="Bold">Mean Diameter</Paragraph></TableCell>
+          <TableCell><Paragraph FontSize="14pt" FontWeight="Bold">Approximate Mass</Paragraph></TableCell>
+        </TableRow>
+
+        <!-- Sub-title row for the inner planets. -->
+        <TableRow>
+          <TableCell ColumnSpan="4"><Paragraph FontSize="14pt" FontWeight="Bold">The Inner Planets</Paragraph></TableCell>
+        </TableRow>
+
+        <!-- Four data rows for the inner planets. -->
+        <TableRow>
+          <TableCell><Paragraph>Mercury</Paragraph></TableCell>
+          <TableCell><Paragraph>57,910,000 km</Paragraph></TableCell>
+          <TableCell><Paragraph>4,880 km</Paragraph></TableCell>
+          <TableCell><Paragraph>3.30e23 kg</Paragraph></TableCell>
+        </TableRow>
+        <TableRow Background="lightgray">
+          <TableCell><Paragraph>Venus</Paragraph></TableCell>
+          <TableCell><Paragraph>108,200,000 km</Paragraph></TableCell>
+          <TableCell><Paragraph>12,103.6 km</Paragraph></TableCell>
+          <TableCell><Paragraph>4.869e24 kg</Paragraph></TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell><Paragraph>Earth</Paragraph></TableCell>
+          <TableCell><Paragraph>149,600,000 km</Paragraph></TableCell>
+          <TableCell><Paragraph>12,756.3 km</Paragraph></TableCell>
+          <TableCell><Paragraph>5.972e24 kg</Paragraph></TableCell>
+        </TableRow>
+        <TableRow Background="lightgray">
+          <TableCell><Paragraph>Mars</Paragraph></TableCell>
+          <TableCell><Paragraph>227,940,000 km</Paragraph></TableCell>
+          <TableCell><Paragraph>6,794 km</Paragraph></TableCell>
+          <TableCell><Paragraph>6.4219e23 kg</Paragraph></TableCell>
+        </TableRow>
+
+        <!-- Sub-title row for the outter planets. -->
+        <TableRow>
+          <TableCell ColumnSpan="4"><Paragraph FontSize="14pt" FontWeight="Bold">The Major Outer Planets</Paragraph></TableCell>
+        </TableRow>
+
+        <!-- Four data rows for the major outter planets. -->
+        <TableRow>
+          <TableCell><Paragraph>Jupiter</Paragraph></TableCell>
+          <TableCell><Paragraph>778,330,000 km</Paragraph></TableCell>
+          <TableCell><Paragraph>142,984 km</Paragraph></TableCell>
+          <TableCell><Paragraph>1.900e27 kg</Paragraph></TableCell>
+        </TableRow>
+        <TableRow Background="lightgray">
+          <TableCell><Paragraph>Saturn</Paragraph></TableCell>
+          <TableCell><Paragraph>1,429,400,000 km</Paragraph></TableCell>
+          <TableCell><Paragraph>120,536 km</Paragraph></TableCell>
+          <TableCell><Paragraph>5.68e26 kg</Paragraph></TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell><Paragraph>Uranus</Paragraph></TableCell>
+          <TableCell><Paragraph>2,870,990,000 km</Paragraph></TableCell>
+          <TableCell><Paragraph>51,118 km</Paragraph></TableCell>
+          <TableCell><Paragraph>8.683e25 kg</Paragraph></TableCell>
+        </TableRow>
+        <TableRow Background="lightgray">
+          <TableCell><Paragraph>Neptune</Paragraph></TableCell>
+          <TableCell><Paragraph>4,504,000,000 km</Paragraph></TableCell>
+          <TableCell><Paragraph>49,532 km</Paragraph></TableCell>
+          <TableCell><Paragraph>1.0247e26 kg</Paragraph></TableCell>
+        </TableRow>
+
+        <!-- Footer row for the table. -->
+        <TableRow>
+          <TableCell ColumnSpan="4"><Paragraph FontSize="10pt" FontStyle="Italic">
+            Information from the 
+            <Hyperlink NavigateUri="http://encarta.msn.com/encnet/refpages/artcenter.aspx">Encarta</Hyperlink> 
+            web site.
+            </Paragraph></TableCell>
+        </TableRow>
+      
+      </TableRowGroup>
+    </Table>
+  </FlowDocument>
+"@ #>
+<# [xml]$doc=@"
+<FlowDocument xml:space="preserve" xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+    FontFamily="Cambria" FontSize="14">
+    <Paragraph>Some Pixar films:</Paragraph>
+    <Table>
+        <Table.Columns>
+            <TableColumn Width="50*"/>
+            <TableColumn Width="15*"/>
+            <TableColumn />
+        </Table.Columns>
+ 
+        <TableRowGroup Background="LightGray" FontWeight="Bold">
+            <TableRow>
+                <TableCell ToolTip="Hello"><Paragraph>Title</Paragraph></TableCell>
+                <TableCell><Paragraph>Year</Paragraph></TableCell>
+                <TableCell><Paragraph>Director</Paragraph></TableCell>
+            </TableRow>
+        </TableRowGroup>
+ 
+        <TableRowGroup FontFamily="Verdana" FontSize="12">
+            <TableRow>
+                <TableCell><Paragraph>Toy Story</Paragraph></TableCell>
+                <TableCell><Paragraph>1995</Paragraph></TableCell>
+                <TableCell><Paragraph>John Lasseter</Paragraph></TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell><Paragraph>Monster's, Inc.</Paragraph></TableCell>
+                <TableCell><Paragraph>2001</Paragraph></TableCell>
+                <TableCell><Paragraph>Pete Docter</Paragraph></TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell><Paragraph>The Incredibles</Paragraph></TableCell>
+                <TableCell><Paragraph>2004</Paragraph></TableCell>
+                <TableCell><Paragraph>Brad Bird</Paragraph></TableCell>
+            </TableRow>
+        </TableRowGroup>
+    </Table>
+</FlowDocument>
+"@
+ #>
 
 Add-Type -AssemblyName PresentationFramework, PresentationCore, WindowsBase, System.Windows.Forms, System.Drawing
 $FormReader = New-Object System.Xml.XmlNodeReader $XAML
@@ -64,21 +208,77 @@ param (
     }  
 }
 
+Function ConvertTo-FlowDocument{
+    $pre = "<FlowDocument xml:space=`"preserve`" xmlns=`"http://schemas.microsoft.com/winfx/2006/xaml/presentation`"
+    FontFamily=`"Tahoma`" FontSize=`"10`">"
+    $table = "<Paragraph FontFamily=`"Segoe UI`" FontSize=`"18`">Sample Output</Paragraph><Table><Table.Columns>"
+    
+    #$object = Get-Service | Select-Object Name,Status,StartType,DisplayName
+    $object = Get-ChildItem | Select LastWriteTime, Length,Name,FullName
+    $props = $object[0].PSObject.Properties.Name
+    
+    $table += $props | % { "<TableColumn/>"}
+    $table += "</Table.Columns><TableRowGroup><TableRow FontFamily=`"Consolas`" Background=`"LightGray`" FontWeight=`"Bold`">"
+    $table += Foreach ($p in $props){
+                "<TableCell><Paragraph>$p</Paragraph></TableCell>"
+            }
+    $table +="</TableRow>"
+    
+    $table += foreach ($obj in $object){
+        $cell = "<TableRow>"
+        $cell += Foreach ($p in $props){
+            if ($p -eq 'Status'){
+                if ($($obj.$p) -ne "Running"){
+                    "<TableCell Foreground=`"Red`"><Paragraph>$($obj.$p)</Paragraph></TableCell>"
+                }else{
+                    "<TableCell><Paragraph>$($obj.$p)</Paragraph></TableCell>"
+                }
+            }else{
+                "<TableCell><Paragraph>$($obj.$p)</Paragraph></TableCell>"
+            }
+        }
+        $cell += "</TableRow>"
+        $cell
+    }
+    
+    $table +="</TableRowGroup></Table></FlowDocument>"
+    [xml]$result = $pre+$table
+    Write-Output $result
+}
 
 $Btn.Add_Click({
+  $ser = Get-Service | select Name,Status | Out-String -Stream
+  $datatable = [System.Data.DataTable]::new()
+  $props = $services[0].PSObject.Properties.Name
+  $props | ForEach-Object { $null = $datatable.Columns.Add($PSItem)}
+  $datatable.Rows.Clear()
+  
+  Foreach ($s in $services){
+      $newrow = $datatable.NewRow()
+         $props| % { $newrow.Item($psitem)=if ($s.$PSItem -ne $null){$s.$PSItem} }
+      $datatable.Rows.Add($newrow)
+  }
+  $TextRange = [System.Windows.Documents.TextRange]::new($rtxt.Document.ContentEnd,$rtxt.Document.ContentEnd)
+  $TextRange.Text = $Ser[1]
+  $TextRange.ApplyPropertyValue([System.Windows.Documents.TextElement]::FontWeightProperty,"Bold")
+  $TextRange.ApplyPropertyValue([System.Windows.Documents.TextElement]::ForegroundProperty,"Red")   
+  
+       
+  3..$ser.Length | %{ 
     $TextRange = [System.Windows.Documents.TextRange]::new($rtxt.Document.ContentEnd,$rtxt.Document.ContentEnd)
-    #$TextRange.ApplyPropertyValue([System.Windows.Documents.TextElement]::FontWeightProperty,"Normal")  
-    $TextRange.Text = "Hello Guys `n"
-    
-    
-    $TextRange = [System.Windows.Documents.TextRange]::new($rtxt.Document.ContentEnd,$rtxt.Document.ContentEnd)
+    $TextRange.Text = "`n$($Ser[$_])" 
+    $TextRange.ApplyPropertyValue([System.Windows.Documents.TextElement]::ForegroundProperty,"Black")
+    $TextRange.ApplyPropertyValue([System.Windows.Documents.TextElement]::FontWeightProperty,[System.Windows.FontWeights]::Regular)  
+    }
+    <# $TextRange = [System.Windows.Documents.TextRange]::new($rtxt.Document.ContentEnd,$rtxt.Document.ContentEnd)
     $TextRange.Text = "Welcome"
-    $TextRange.ApplyPropertyValue([System.Windows.Documents.TextElement]::ForegroundProperty,"Red")  
+    $TextRange.ApplyPropertyValue([System.Windows.Documents.TextElement]::FontWeightProperty,"Bold")  
+    $TextRange.ApplyPropertyValue([System.Windows.Documents.TextElement]::ForegroundProperty,"Red")  #> 
     
 })
 
 $NxtBtn.Add_Click({
-  $flowdoc =[System.Windows.Documents.FlowDocument]::new()
+  <# $flowdoc =[System.Windows.Documents.FlowDocument]::new()
   $para = [System.Windows.Documents.Paragraph]::new()
   $para.Inlines.Add("Hello Bro")
   $bold = [System.Windows.Documents.Bold]::new()
@@ -97,8 +297,12 @@ $NxtBtn.Add_Click({
  
   $flowdoc.Blocks.Add($para1)
   $flowdoc.Blocks.Add($table)
-  $Rtxt.Document = $flowdoc
-
+  $Rtxt.Document = $flowdoc #>
+    $doc = ConvertTo-FlowDocument
+    $DocReader = New-Object System.Xml.XmlNodeReader $doc
+    $flowdoc = [Windows.MarkUp.XamlReader]::Load($DocReader)
+    $Rtxt.Document = $flowdoc 
+    #$fldocument =[System.Windows.Documents.FlowDocument]::new()
 })
 
 $WebForm.ShowDialog()
--- ShouldProcessExample.ps1
+++ ShouldProcessExample.ps1
@@ -21,38 +21,8 @@ Show-Parameter -Text "hee" -Number "10" -Verbose
 #Get-Item Function:\Show-Parameter | Remove-Item    
 
 
-$disk =gci -force 'C:\Users'-ErrorAction SilentlyContinue | ? { $_ -is [io.directoryinfo] } | % {
+$disk =gci -force 'D:\'-ErrorAction SilentlyContinue | ? { $_ -is [io.directoryinfo] } | % {
     $len = 0
     gci -recurse -force $_.fullname -ErrorAction SilentlyContinue | % { $len += $_.length }
-    [pscustomobject]@{ Name = $_.fullname ;  Size = '{0:N2} GB' -f ($len / 1Gb)  }    
+    [pscustomobject]@{ Name = $_.name ;Path =$_.FullName ; Size = '{0:N2} GB' -f ($len / 1Gb)  }    
 }
-
-[void][Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
-[void][Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms.DataVisualization")
-
-# create chart object
-$Chart = New-object System.Windows.Forms.DataVisualization.Charting.Chart
-$Chart.Width = 500
-$Chart.Height = 400
-$Chart.Left = 40
-$Chart.Top = 30
-
-# create a chartarea to draw on and add to chart
-$ChartArea = New-Object System.Windows.Forms.DataVisualization.Charting.ChartArea
-$Chart.ChartAreas.Add($ChartArea)
-$Cities = @{London=7556900; Berlin=3429900; Madrid=3213271; Rome=2726539;
-    Paris=2188500}
-[void]$Chart.Series.Add("Data")
-$Chart.Series["Data"].Points.DataBindXY($Cities.Keys, $Cities.Values)
-<# [void]$Chart.Series.Add("Data")
-$Chart.Series["Data"].Points.DataBindXY($disk.Name, $disk.Size) #>
-
-$Chart.Anchor = [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Right -bor
-                [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left
-$Form = New-Object Windows.Forms.Form
-$Form.Text = "PowerShell Chart"
-$Form.Width = 600
-$Form.Height = 600
-$Form.controls.add($Chart)
-$Form.Add_Shown({$Form.Activate()})
-$Form.ShowDialog()
